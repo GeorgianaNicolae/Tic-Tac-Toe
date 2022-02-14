44b71a3e1aa461f36ad33eb44f922e394bb09f13
GeorgianaNicolae <georgiana.nicolae@madetech.com>
2022-02-14 16:37:56 +0000

    first failing test for an empty array

---
+++ spec/testing_spec.rb
@@ -0,0 +1,15 @@
+require "./tictactoe"
+
+describe "TicTacToe" do 
+    before (:each) do
+        @tictactoe = described_class.new
+    end
+
+    it "returns an empty array" do
+        # Act
+        result = @tictactoe.get_grid()
+
+        # Assert
+        expect(result).to eq([])
+    end 
+end 
