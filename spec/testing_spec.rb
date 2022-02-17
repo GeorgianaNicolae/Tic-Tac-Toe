448c19fe7690fa2986ed1bb0aaacff2e558898f3
Rachel Howell <rachel.howell@madetech.com>
2022-02-17 15:11:06 +0000

    delete first test and add new test to join the arrays with a pipe and return a string then introduce a new line after each row to look like a grid

--- spec/testing_spec.rb
+++ spec/testing_spec.rb
@@ -5,19 +5,19 @@ describe TicTacToe do
         @tictactoe = described_class.new
     end
 
-    it "returns 3 arrays with 3 nil values in each array within an array" do
-        # Act
-        result = @tictactoe.get_grid()
-
-        # Assert
-        expect(result).to eq([[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]])
-    end 
-
     it "starts the game by receiving the name of the player as a string and places an 'X' in the top left of the grid" do
         # Act
-        result = @tictactoe.update_grid("Malcolm")
+        result = @tictactoe.update_grid("Malcome")
 
         # Assert
-        expect(result).to eq([["X", nil, nil], [nil, nil, nil], [nil, nil, nil]])
+        expect(result).to eq([["X", " ", " "], [" "," "," "], [" "," "," "]])
+    end 
+
+    it "joins the arrays with a pipe and returns a string then introduces a new line after each row to look like a grid" do
+        # Act
+        result = @tictactoe.draw_grid("Malcome")
+
+        # Assert
+        expect(result).to eq("X| | \n | | \n | | ")
     end 
 end
